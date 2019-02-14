0def2a49e394a65fdc1853c0e9c73d5537f752bd
Jose Zalacain <Jose Zalacain@10.11.0.30>
2019-02-14 18:12:19 -0400

    Change new provider to get provider

--- src/test/java/org/prolobjectlink/prolog/jtrolog/PrologBaseTest.java
+++ src/test/java/org/prolobjectlink/prolog/jtrolog/PrologBaseTest.java
@@ -51,7 +51,7 @@ public abstract class PrologBaseTest {
 	protected String employee = "employee";
 	protected String department = "department";
 
-	protected static final PrologProvider provider = Prolog.newProvider(JTrolog.class);
+	protected static final PrologProvider provider = Prolog.getProvider(JTrolog.class);
 
 	protected PrologTerm nil = provider.prologNil();
 	protected PrologTerm empty = provider.prologEmpty();
