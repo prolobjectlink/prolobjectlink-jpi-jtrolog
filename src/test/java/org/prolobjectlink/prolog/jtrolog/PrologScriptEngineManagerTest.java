733167b619d138d23fa13d6e5d93cb2ae281d588
Jose Zalacain <Jose Zalacain@10.11.0.30>
2019-05-05 16:32:16 -0400

    javax.script

--- src/test/java/org/prolobjectlink/prolog/jtrolog/PrologScriptEngineManagerTest.java
+++ src/test/java/org/prolobjectlink/prolog/jtrolog/PrologScriptEngineManagerTest.java
@@ -35,7 +35,15 @@ public class PrologScriptEngineManagerTest extends PrologBaseTest {
 	public void test() throws ScriptException {
 
 		ScriptEngineManager manager = new ScriptEngineManager();
-		ScriptEngine engine = manager.getEngineByName(provider.newEngine().getName());
+		ScriptEngine engine = manager.getEngineByName(provider.getName());
+		assertEquals(true, engine.eval("?- X is 5+3."));
+		assertEquals(provider.newInteger(8), engine.get("X"));
+
+		engine = manager.getEngineByName("Prolog");
+		assertEquals(true, engine.eval("?- X is 5+3."));
+		assertEquals(provider.newInteger(8), engine.get("X"));
+
+		engine = manager.getEngineByName("prolog");
 		assertEquals(true, engine.eval("?- X is 5+3."));
 		assertEquals(provider.newInteger(8), engine.get("X"));
 
