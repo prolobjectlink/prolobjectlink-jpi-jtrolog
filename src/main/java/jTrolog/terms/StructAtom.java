9ec6a918ff5a713b84f0e754bf12752cde2ad021
Jose Zalacain <Jose Zalacain@XEON>
2019-07-28 20:42:55 -0400

    no java doc generation

--- src/main/java/jTrolog/terms/StructAtom.java
+++ src/main/java/jTrolog/terms/StructAtom.java
@@ -1,12 +1,12 @@
 /*
  * #%L
- * prolobjectlink-jtrolog
+ * prolobjectlink-jpi-jtrolog
  * %%
  * Copyright (C) 2012 - 2018 WorkLogic Project
  * %%
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 3 of the 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
@@ -14,9 +14,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
  * 
- * You should have received a copy of the GNU General Lesser Public 
+ * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
  * #L%
  */
 package jTrolog.terms;
